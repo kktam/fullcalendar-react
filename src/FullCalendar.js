a0b9a5e97426b1dfa022a2b5081c6c7ebf529b19
Nelson Tam <nkltam2003@gmail.com>
2017-07-02 18:33:32 -0700

    Add list view and parameters for events

--- src/FullCalendar.js
+++ src/FullCalendar.js
@@ -6,11 +6,29 @@ import 'fullcalendar'
 import 'fullcalendar/dist/fullcalendar.css'
 
 class FullCalendar extends Component {
+
     componentDidMount() {
         const { calendar } = this.refs;
 
         this._calendar = $(calendar);
-        this._calendar.fullCalendar();
+        this._calendar.fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'listDay,listWeek,month'
+			},
+
+			// customize the button names,
+			// otherwise they'd all just say "list"
+			views: {
+				listDay: { buttonText: 'list day' },
+				listWeek: { buttonText: 'list week' }
+			},
+
+			defaultView: 'month',
+            
+            events: this.props.events,
+        });
     }
 
     render() {
@@ -19,6 +37,22 @@ class FullCalendar extends Component {
         );
     }
 }
+
+FullCalendar.propTypes = {
+  onHover: PropTypes.func,
+  onClick: PropTypes.func,
+  size: PropTypes.shape({
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired      
+  }),
+  events: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.number.isRequired,
+    start: PropTypes.instanceOf(Date).isRequired,
+    end: PropTypes.instanceOf(Date),
+    url: PropTypes.string
+  }))
+};
 
 export default FullCalendar
 
